085faf05d15aea0a9ca5d7be863502fb3d8b70f9
Ivan Daunis <ivan.daunis@retailnext.net>
2020-08-19 17:42:22 -0700

    Implement ssh and scp via gcloud for ansible

---
+++ main.go
@@ -0,0 +1,303 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"golang.org/x/oauth2/google"
+	compute "google.golang.org/api/compute/v1"
+)
+
+var (
+	errorHasIdentityFile = errors.New("Has identity file")
+)
+
+// finds the project, zone and instance name that belongs to a networkIP
+func findInstance(computeService *compute.Service, projects, zones []string, networkIP string) (string, string, string, error) {
+	for _, project := range projects {
+		projectZones := zones
+		if len(projectZones) == 0 {
+			// If no zones are specified list all available zones
+			zoneListCall := computeService.Zones.List(project)
+			zoneList, err := zoneListCall.Do()
+			if err != nil {
+				return "", "", "", err
+			}
+			for _, zone := range zoneList.Items {
+				projectZones = append(projectZones, zone.Name)
+			}
+		}
+
+		for _, zone := range projectZones {
+			instanceListCall := computeService.Instances.List(project, zone)
+			instanceListCall.Filter("(status = RUNNING)")
+			instanceList, err := instanceListCall.Do()
+			if err != nil {
+				return "", "", "", err
+			}
+
+			for _, instance := range instanceList.Items {
+				for _, ni := range instance.NetworkInterfaces {
+					if ni.NetworkIP == networkIP {
+						log.Printf("Found network IP: %s in zone: %s with name: %s", networkIP, zone, instance.Name)
+						return instance.Name, zone, project, nil
+					}
+				}
+			}
+		}
+	}
+
+	return "", "", "", fmt.Errorf("Not found networkIP: %v", networkIP)
+}
+
+func runGCloudSSH(ar AnsibleRun) error {
+	cmd := exec.Command("gcloud",
+		"compute",
+		"ssh",
+		"--quiet",
+		"--tunnel-through-iap",
+		"--project", ar.Project,
+		"--zone", ar.Zone, ar.Destination,
+		"--command", ar.Command,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func runGCloudSCP(ar AnsibleRun) error {
+	cmd := exec.Command("gcloud",
+		"compute",
+		"scp",
+		"--quiet",
+		"--tunnel-through-iap",
+		"--project", ar.Project,
+		"--zone", ar.Zone, ar.Source, ar.Destination,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func runSystemSCP(args []string) error {
+	log.Println("Running system-scp with args:", args)
+	cmd := exec.Command("system-scp", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+type AnsibleRun struct {
+	Command     string
+	Source      string
+	Destination string
+	Zone        string
+	Project     string
+
+	Options []string
+}
+
+func ParseAnsibleArgs(args []string) (AnsibleRun, error) {
+	result := AnsibleRun{}
+	commands := []string{}
+	for i := 1; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-c":
+			i++
+			result.Command = args[i]
+			continue
+		case "-o":
+			i++
+			result.Options = append(result.Options, args[i])
+			continue
+		default:
+			if arg[0] == '-' {
+				continue
+			}
+		}
+
+		if result.Destination == "" {
+			result.Destination = arg
+		} else {
+			commands = append(commands, arg)
+		}
+	}
+
+	if result.Destination == "" {
+		return result, fmt.Errorf("Empty destination")
+	}
+	if result.Command == "" {
+		result.Command = strings.Join(commands, " ")
+	}
+	if result.Command == "" {
+		return result, fmt.Errorf("Empty command")
+	}
+	log.Printf("Parsed ansible ssh: %#+v", result)
+	return result, nil
+}
+
+func ParseAnsibleSCP(args []string) (AnsibleRun, error) {
+	result := AnsibleRun{}
+	for i := 1; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-i":
+			return result, errorHasIdentityFile
+		case "-o":
+			i++
+			result.Options = append(result.Options, args[i])
+			continue
+		default:
+			if arg[0] == '-' {
+				continue
+			}
+		}
+		if result.Source == "" {
+			result.Source = arg
+			continue
+		}
+		result.Destination = arg
+	}
+
+	if result.Destination == "" {
+		return result, fmt.Errorf("Empty destination")
+	}
+	if result.Source == "" {
+		return result, fmt.Errorf("Empty source")
+	}
+	log.Printf("Parsed ansible scp: %#+v", result)
+	return result, nil
+}
+
+func ExtractIP(str string) string {
+	// SCP destination is [xxx]:yyy
+	parts := strings.Split(str, ":")
+	wrapped := parts[0]
+	if strings.Index(wrapped, "[") == 0 {
+		return wrapped[1 : len(wrapped)-1]
+	}
+	return parts[0]
+}
+
+func updateWithInstanceName(projects, zones []string, ansible *AnsibleRun) error {
+	ctx := context.Background()
+	client, err := google.DefaultClient(ctx, compute.ComputeScope)
+	if err != nil {
+		return err
+	}
+
+	networkIP := ExtractIP(ansible.Destination)
+
+	computeService, err := compute.New(client)
+	if err != nil {
+		return err
+	}
+	instanceName, zone, project, err := findInstance(computeService, projects, zones, networkIP)
+	if err != nil {
+		return err
+	}
+
+	if strings.Index(ansible.Destination, "[") == 0 {
+		ansible.Destination = strings.Replace(ansible.Destination, "["+networkIP+"]", instanceName, -1)
+	} else {
+		ansible.Destination = strings.Replace(ansible.Destination, networkIP, instanceName, -1)
+	}
+	ansible.Zone = zone
+	ansible.Project = project
+
+	return nil
+}
+
+func setupLogger() func() {
+	f, err := os.OpenFile("/var/log/gcloud-ssh.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+
+	return func() {
+		f.Close()
+	}
+}
+
+// Get env var or default
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+// Get env var or default
+func getEnvList(key string, fallback []string) []string {
+	if value, ok := os.LookupEnv(key); ok {
+		if value != "" {
+			return strings.Split(value, ",")
+		}
+	}
+	return fallback
+}
+
+func parseAndRun(doSCP bool, projects, zones []string) error {
+	if doSCP {
+		// Check if we have to run system's scp command
+		ansible, err := ParseAnsibleSCP(os.Args)
+		if err != nil {
+			if err == errorHasIdentityFile {
+				err = runSystemSCP(os.Args[1:])
+			}
+			return err
+		}
+
+		// Running Cloud SCP
+		err = updateWithInstanceName(projects, zones, &ansible)
+		if err != nil {
+			return err
+		}
+		runGCloudSCP(ansible)
+	} else {
+		ansible, err := ParseAnsibleArgs(os.Args)
+		if err != nil {
+			return err
+		}
+
+		err = updateWithInstanceName(projects, zones, &ansible)
+		if err != nil {
+			return err
+		}
+		runGCloudSSH(ansible)
+	}
+	return nil
+}
+
+func main() {
+	closeLogger := setupLogger()
+	defer closeLogger()
+
+	doSCP, _ := strconv.ParseBool(getEnv("DO_SCP", "false"))
+	zones := getEnvList("GCLOUD_SSH_ZONES", []string{})
+	projects := getEnvList("GCLOUD_SSH_PROJECTS", []string{})
+
+	if len(projects) == 0 {
+		ctx := context.Background()
+		credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeScope)
+		if err != nil {
+			log.Fatal(err)
+		}
+		projects = append(projects, credentials.ProjectID)
+	}
+	log.Printf("Starting with zones: %v, projects: %v, doSCP: %v", zones, projects, doSCP)
+
+	err := parseAndRun(doSCP, projects, zones)
+	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	argState    = 1
+	startState  = 2
+	quotesState = 3
+)
+
+func parseCommandLine(command string) ([]string, error) {
+	var args []string
+	var quote byte = '"'
+	state := startState
+	current := ""
+	escapeNext := true
+	for i := 0; i < len(command); i++ {
+		c := command[i]
+
+		if state == quotesState {
+			if c != quote {
+				current += string(c)
+			} else {
+				args = append(args, current)
+				current = ""
+				state = startState
+			}
+			continue
+		}
+
+		if escapeNext {
+			current += string(c)
+			escapeNext = false
+			continue
+		}
+
+		if c == '\\' {
+			escapeNext = true
+			continue
+		}
+
+		if c == '"' || c == '\'' {
+			state = quotesState
+			quote = c
+			continue
+		}
+
+		if state == argState {
+			if c == ' ' || c == '\t' {
+				args = append(args, current)
+				current = ""
+				state = startState
+			} else {
+				current += string(c)
+			}
+			continue
+		}
+
+		if c != ' ' && c != '\t' {
+			state = argState
+			current += string(c)
+		}
+	}
+
+	if state == quotesState {
+		return []string{}, fmt.Errorf("Unclosed quote in command line: %s", command)
+	}
+
+	if current != "" {
+		args = append(args, current)
+	}
+
+	return args, nil
+}
+
+func TestSSH(t *testing.T) {
+	// gcloud compute ssh --tunnel-through-iap --quiet --zone "us-central1-a" "andy-awx-managed-1" --command "ls"
+	args, err := parseCommandLine(`-C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o User="andy_retailnext_net" -o ConnectTimeout=10 -o ControlPath=/tmp/awx_2826_wrtrtczl/cp/3c85463f3f 172.16.0.12 /bin/sh -c '/usr/bin/python3 && sleep 0'`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	a, err := ParseAnsibleArgs(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := "/usr/bin/python3 && sleep 0"
+	if a.Command != expected {
+		t.Fatalf("'%v' != '%v'", a.Command, expected)
+	}
+
+	args2, err := parseCommandLine(`-C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o User="sa_111069622966946909314" -o ConnectTimeout=10 -o ControlPath=/tmp/awx_2848_uq4itrrn/cp/811b91f774 172.16.0.11 dd of=/home/sa_111069622966946909314/.ansible/tmp/ansible-tmp-1596502037.623799-215726-161858982386430/AnsiballZ_setup.py bs=65536`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	a, err = ParseAnsibleArgs(args2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected = "dd of=/home/sa_111069622966946909314/.ansible/tmp/ansible-tmp-1596502037.623799-215726-161858982386430/AnsiballZ_setup.py bs=65536"
+	if a.Command != expected {
+		t.Fatalf("'%v' != '%v'", a.Command, expected)
+	}
+	if a.Destination != "172.16.0.11" {
+		t.Fatalf("'%v' != '%v'", a.Destination, "172.16.0.11")
+	}
+	if a.Source != "" {
+		t.Fatalf("destination must be empty, found '%v'", a.Source)
+	}
+}
+
+func TestSCP(t *testing.T) {
+	args3, err := parseCommandLine(`-C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o User="sa_111069622966946909314" -o ConnectTimeout=10 -o ControlPath=/tmp/awx_2850_mcg_tln7/cp/811b91f774 /var/lib/awx/.ansible/tmp/ansible-local-216033jdy7a18f/tmpja9h4a0t [172.16.0.11]:/home/sa_111069622966946909314/.ansible/tmp/ansible-tmp-1596502613.2008872-216047-259015317780472/AnsiballZ_setup.py`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	a, err := ParseAnsibleSCP(args3)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if a.Command != "" {
+		t.Fatalf("command must be empty, found '%v'", a.Command)
+	}
+	expected := `/var/lib/awx/.ansible/tmp/ansible-local-216033jdy7a18f/tmpja9h4a0t`
+	if a.Source != expected {
+		t.Fatalf("'%v' != '%v'", a.Source, expected)
+	}
+	expected = `[172.16.0.11]:/home/sa_111069622966946909314/.ansible/tmp/ansible-tmp-1596502613.2008872-216047-259015317780472/AnsiballZ_setup.py`
+	if a.Destination != expected {
+		t.Fatalf("'%v' != '%v'", a.Destination, expected)
+	}
+
+	expected = "172.16.0.11"
+	ip := ExtractIP(a.Destination)
+	if ip != expected {
+		t.Fatalf("'%v' != '%v'", ip, expected)
+	}
+
+	ip = ExtractIP(expected)
+	if ip != expected {
+		t.Fatalf("'%v' != '%v'", ip, expected)
+	}
+}
